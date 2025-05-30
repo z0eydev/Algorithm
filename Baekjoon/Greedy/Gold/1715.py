481993e680ac1433eb9af0e9cd16a7aa002e0590
z0eydev <yoojung980611@gmail.com>
2025-05-30 13:16:55 +0900

    250530

---
+++ Baekjoon/Greedy/Gold/1715.py
@@ -0,0 +1,17 @@
+# 백준 gold4 카드 정렬하기
+from heapq import heappop, heappush
+
+N = int(input())
+card_list = []
+for _ in range(N):
+    heappush(card_list, int(input()))
+result = 0
+
+while len(card_list) > 1:
+    first = heappop(card_list)
+    second = heappop(card_list)
+    bundle = first + second
+    result += bundle
+    heappush(card_list, bundle)
+
+print(result)
